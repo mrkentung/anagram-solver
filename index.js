8db5778998d89aafed2d9e0ea495a8fbc3eef976
mrkentung <widiyanto.adhi@gmail.com>
2021-06-18 15:29:10 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const indexWord = (word) => {
+
+// count occurence each character
+const x = {}
+
+for (let i = 0; i < word.length; i++) {
+	const c = word[i]
+	if (x[c]) {
+		x[c]++
+	} else {
+		x[c] = 1
+	}
+}
+
+// build index
+const sortedKeys = Object.keys(x).sort()
+	let result = ''
+	for (let i = 0; i < sortedKeys.length; i++) {
+		result += `${sortedKeys[i]}${x[sortedKeys[i]]}`
+	}
+
+	return result
+}
+
+const main = (arr) => {
+	const result = {}
+	
+	for (let i = 0; i < arr.length; i++) {
+		const word = arr[i]
+		const index = indexWord(word)
+		if (result[index]) {
+			result[index].push(word)
+		} else {
+			result[index] = [word]
+		}
+	}
+
+	const arrOfValues = Object.values(result)
+
+	return arrOfValues
+}
+
+const input = [
+	'ayam',
+	'maya',
+	'maya',
+	'telur',
+	'relut',
+	'keluarga',
+	'aku',
+	'diam',
+	'garaleuk',
+]
+
+const answer = main(input)
+
+console.log(answer)
